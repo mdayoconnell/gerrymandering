a08d96c67e8a01383d0a197174f3b8743fcf032f
mdayoconnell <mdayoconnell@gmail.com>
2026-03-26 13:03:35 +0100

    moved temperature to ui

--- user_interface/__init__.py
+++ user_interface/__init__.py
@@ -1,9 +1,11 @@
 from user_interface.custom_loss_ui import (
     PUBLISHED_WEIGHTS,
     get_loss_weights,
+    get_uphill_move_probability,
     launch_loss_weight_sliders,
     publish_loss_weights,
     set_loss_weights,
+    set_uphill_move_probability,
 )
 from user_interface.flip_history_ui import FlipHistoryFrame, launch_flip_history_viewer
 
@@ -11,8 +13,10 @@ __all__ = [
     "PUBLISHED_WEIGHTS",
     "FlipHistoryFrame",
     "get_loss_weights",
+    "get_uphill_move_probability",
     "launch_flip_history_viewer",
     "launch_loss_weight_sliders",
     "publish_loss_weights",
     "set_loss_weights",
+    "set_uphill_move_probability",
 ]
